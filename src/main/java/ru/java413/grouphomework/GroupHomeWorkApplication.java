c04e4230b3266544f1b752d2053c01851ef5c78e
Дмитрий <disturbeddokubo25@gmail.com>
2025-10-20 23:31:56 +0300

    First commit

---
+++ src/main/java/ru/java413/grouphomework/GroupHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package ru.java413.grouphomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupHomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/java413/grouphomework/GroupHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java413.grouphomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
