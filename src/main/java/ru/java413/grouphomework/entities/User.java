b92b1b67e7471dfd5a65554d45dec755050a8c77
Дмитрий <Dodge2510@mail.ru>
2025-10-21 20:16:02 +0300

    убрал размерность поля, т.к. при регистрации шифрование пароля может быть более 50 символов

--- src/main/java/ru/java413/grouphomework/entities/User.java
+++ src/main/java/ru/java413/grouphomework/entities/User.java
@@ -13,7 +13,7 @@ public class User {
     @Column(unique = true, nullable = false, length = 50)
     private String username;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false)
     private String password;
 
     @Column(unique = true, nullable = false, length = 100)
